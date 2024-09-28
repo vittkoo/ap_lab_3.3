67c20424ce3c59ef8a1c23b9c9fceb82d189501f
vittkoo <dmytrovych.vv@gmail.com>
2024-09-28 10:19:00 +0300

    commit_2

--- lab_3.3.cpp
+++ lab_3.3.cpp
@@ -6,32 +6,38 @@
 
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 int main()
 {
     double x;
     double R;
-    double y;
+    double y; 
 
-    cout << "Enter R: "; cin >> R;
-    cout << "Enter x: "; cin >> x;
+    cout << "R = ";
+    cin >> R;
+    cout << "x = ";
+    cin >> x;
 
     // розгалуження в повній формі
     if (x <= -R)
-        y = R;
+        y = R;  // горизонтальна лінія при x <= -R
+
+    else if (x > -R && x <= R)
+        y = -sqrt(R * R - (x + R) * (x + R));  // півколо з центром у (-R,R)
+
+    else if (x > R && x <= 6)
+        y = -R * (x - R) / (6 - R);  // спадна лінія від (R,0) до (6,-3)
+
+    else if (x > 6 && x <= 9)
+        y = (x - 6) - 3;  // лінія зростання від (6,-3) до (9,0)
+
     else
-        if (-R < x && x <= R)
-            y = -sqrt(R * R - x * x); // частина кола
-        else
-            if (R < x && x <= 6)
-                y = -3 * (x - R) / (6 - R); // лінійна ділянка між R та 6
-            else
-                y = (x - 9) / 3; // лінійна ділянка після 6
+        y = x - 9;  // лінія після x = 9
 
     cout << endl;
     cout << "y = " << y << endl;
 
-    cin.get();
     return 0;
 }
